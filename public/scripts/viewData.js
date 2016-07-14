edae3d4e79819313e61956ed4988749b5961647a
JESSICA VASQUEZ-SOLTERO <JMICHELLEVS@GMAIL.COM>
2016-07-14 15:55:12 -0700

    Changed result to searchTerms

--- public/scripts/viewData.js
+++ public/scripts/viewData.js
@@ -2,8 +2,8 @@ var viewData = {};
 
 $('#searchBox').on('submit', function(e) {
   e.preventDefault();
-  var result = this.search.value;
-  getData.runSearch(result);
+  var searchTerms = this.search.value;
+  getData.runSearch(searchTerms);
 });
 
 var template = $('#search-results').html();
@@ -11,7 +11,7 @@ var render = Handlebars.compile(template);
 
 viewData.printResults = function(data) {
   data.matches.forEach(function(ele) {
-      console.log(ele);
-      $("#print-results").append(render(ele));
+    console.log(ele);
+    $('#print-results').append(render(ele));
   });
-}
+};
