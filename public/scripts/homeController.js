8f5e8f85a75e0c0dfbda7de0c1d1c60414925add
Lillian Szugyi <lillian.szugyi@gmail.com>
2016-07-17 10:54:38 -0700

    uncommented DB test

--- public/scripts/homeController.js
+++ public/scripts/homeController.js
@@ -6,14 +6,14 @@
     $('#login-signup').hide();
   };
 
-  // homeController.dbTest = function(){
-  //   $.getJSON('/data', function (result) {
-  //     result.rows.forEach(function(item) {
-  //       console.log(item);
-  //     });
-  //   });
-  // };
-  // homeController.dbTest();
+  homeController.dbTest = function(){
+    $.getJSON('/data', function (result) {
+      result.rows.forEach(function(item) {
+        console.log(item);
+      });
+    });
+  };
+  homeController.dbTest();
 
   module.homeController = homeController;
 })(window);
