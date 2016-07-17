78653d4d8d80d4ac748c5ad159b0d75599753ba1
Jessica Vasquez-Soltero <jmichellevs@gmail.com>
2016-07-17 00:51:55 -0700

    more server

--- public/scripts/homeController.js
+++ public/scripts/homeController.js
@@ -4,33 +4,22 @@
   homeController.index = function() {
     $('#home').show().siblings().hide();
     $('#login-signup').hide();
-
-    // pg.connect(process.env.DATABASE_URL, function(err, client) {
-    //   if (err) throw err;
-    //   console.log('Connected to postgres! Getting schemas...');
-    //
-    //   client
-    //     .query('SELECT * FROM users;')
-    //     .on('row', function(row) {
-    //       console.log(JSON.stringify(row));
-    //     });
-    // });
-
   };
 
   homeController.fetchAll = function(){
     $.getJSON('/data', function (result) {
       console.log('Scott was here');
-      console.log(result);
-      console.log(result.row);
+      // console.log(result);
+      // console.log(result.row);
 
-      // result.rows.forEach(function(item) {
+      result.rows.forEach(function(item) {
+        console.log(item);
       //   if (item.list){
       //     console.log('if state');
       //     item.list = JSON.parse(item.list);
       //   }
       //   console.log('else');
-      // });
+      });
     });
   };
   homeController.fetchAll();
