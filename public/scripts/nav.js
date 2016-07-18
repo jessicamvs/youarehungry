dbcd763b6f15a7dc4970c56ff3b378dd1dfd72e7
JESSICA VASQUEZ-SOLTERO <JMICHELLEVS@GMAIL.COM>
2016-07-18 10:26:23 -0700

    Little cleanup, login route isnt working on logout

--- public/scripts/nav.js
+++ public/scripts/nav.js
@@ -1,17 +1,8 @@
 $(document).ready(function() {
   $('body').addClass('js');
 
-  var $menu = $('.icon-spoon-knife'),
-    $menulink = $('.menu-link'),
-    $wrap = $('#wrap');
-
-  $menu.on('click', function() {
-    $menulink.toggleClass('active');
-    $wrap.toggleClass('active');
+  $('#mainNav').on('click', 'a', function() {
+    $('.menu-link').toggleClass('active');
+    $('#wrap').toggleClass('active');
   });
-
-$('#menu').on('click', 'li', function() {
-  $menulink.toggleClass('active');
-  $wrap.toggleClass('active');
-})
 });
