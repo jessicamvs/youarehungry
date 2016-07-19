1afa1bfe9587cb82a2320137bd5eb1caba9924dd
Nassir Isaf <njisaf@gmail.com>
2016-07-18 21:08:21 -0700

    New JSON thing

--- public/scripts/listController.js
+++ public/scripts/listController.js
@@ -7,6 +7,20 @@
     listController.dbTest();
   };
 
+  listController.desperateAardvark = function(queryString) {
+    pg.defaults.ssl = true;
+    pg.connect(process.env.DATABASE_URL, function(err, client) {
+      if (err) throw err;
+      console.log('Connected to postgres! Getting schemas...');
+
+      client
+        .query(queryString)
+        .on('row', function(row) {
+          console.log(JSON.stringify(row));
+        });
+    });
+  };
+
   listController.dbTest = function(){
     console.log('listController firing');
     $.getJSON('/ingredients', function (result) {
