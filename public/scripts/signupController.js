2dcdf20a408897a8b0620d6aaf6c1dba77c8d6be
JESSICA VASQUEZ-SOLTERO <JMICHELLEVS@GMAIL.COM>
2016-07-21 15:58:43 -0700

    UserDate in LS on signup

--- public/scripts/signupController.js
+++ public/scripts/signupController.js
@@ -11,17 +11,20 @@
       e.preventDefault();
       console.log(e.target.email.value);
       console.log(e.target.password.value);
-      $.get('/adduser', {email: e.target.email.value, pass: e.target.password.value});
-      // .done(function(result) {
-        // var currentUser = {
-        //   email: result.rows[0].email,
-        //   password: result.rows[0].password
-        // };
-        // localStorage.setItem('userData', JSON.stringify(currentUser));
-      // });
-      page('/search');
+      $.get('/adduser', {email: e.target.email.value, pass: e.target.password.value}).done(function() {
+        $.get('/data', {email: e.target.email.value}).done(function(result) {
+          console.log(result.rows);
+          var currentUser = {
+            id: result.rows[0].id,
+            email: result.rows[0].email
+          };
+          localStorage.setItem('userData', JSON.stringify(currentUser));
+          page('/search');
+        });
+      });
     });
   };
+
 
   module.signupController = signupController;
 })(window);
